b0832070c6ca9a131e25f8e1a6f8d6fe5b03b57c
Julian Herrera <julianeherreraok@gmail.com>
2022-12-06 15:18:38 -0300

    Test and code functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
 function App() {
   const [buttonColor, setButtonColor] = useState("red");
+  const [disabled, setDisabled] = useState(false);
 
   const newButtonColor = buttonColor === "red" ? "blue" : "red";
 
   return (
     <div>
       <button
+        disabled={disabled}
         style={{ backgroundColor: buttonColor }}
         onClick={() => setButtonColor(newButtonColor)}>
         Change to {newButtonColor}
       </button>
+      <input
+        type="checkbox"
+        id="desabled-button-checked"
+        defaultChecked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
     </div>
   );
 }
